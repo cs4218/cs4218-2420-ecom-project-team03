c97fff83890aee87f7fa07514c25fe6d5f3faef9
McNaBry <mcnabry123@gmail.com>
2025-02-17 18:08:08 +0800

    Update unit tests for ProductDetails

--- client/src/pages/ProductDetails.test.js
+++ client/src/pages/ProductDetails.test.js
@@ -1,45 +1,47 @@
 import React from 'react';
-import { screen } from '@testing-library/dom'
-import { render, fireEvent, waitFor } from '@testing-library/react';
+import { render, fireEvent, waitFor, screen } from '@testing-library/react';
 import axios from 'axios';
 import { MemoryRouter, Routes, Route } from 'react-router-dom';
 import '@testing-library/jest-dom/extend-expect';
 import ProductDetails from './ProductDetails';
 
 const LAPTOP = {
-  _id: 'example',
+  _id: '1',
   name: "Laptop",
   slug: "laptop",
   description: "A powerful laptop",
   price: 1499.99,
   category: {
-    _id: "example",
+    _id: "1",
     name: "Electronics",
     slug: "electronics"
-  },
-  quantity: 30,
-  // photo: {
-  //   data: Buffer,
-  // },
-  shipping: true,
+  }
 }
 
 const SMARTPHONE = {
-  _id: 'example',
+  _id: '2',
   name: "Smartphone",
   slug: "smartphone",
   description: "A high-end smartphone",
   price: 999.99,
   category: {
-    _id: "example",
+    _id: "2",
     name: "Electronics",
     slug: "electronics"
-  },
-  quantity: 50,
-  // photo: {
-  //   data: Buffer,
-  // },
-  shipping: false,
+  }
+}
+
+const TELEVISION = {
+  _id: '3',
+  name: "Television",
+  slug: "television",
+  description: "A wide screen television",
+  price: 400,
+  category: {
+    _id: "3",
+    name: "Electronics",
+    slug: "electronics"
+  }
 }
 
 jest.mock('axios');
@@ -70,7 +72,7 @@ describe('Product Details Component', () => {
       }
     }).mockResolvedValueOnce({ data: { products: [ ] } });
 
-    const { getByPlaceholderText, getByText } = render(
+    const { getByText } = render(
       <MemoryRouter initialEntries={['/product/laptop']}>
           <Routes>
               <Route path="/product/:slug" element={<ProductDetails />} />
@@ -114,7 +116,7 @@ describe('Product Details Component', () => {
 
     const imgElement = screen.getByRole("img");
     expect(imgElement).toBeInTheDocument();
-    expect(imgElement).toHaveAttribute("src", "/api/v1/product/product-photo/example");
+    expect(imgElement).toHaveAttribute("src", "/api/v1/product/product-photo/1");
     expect(imgElement).toHaveAttribute("alt", "Laptop");
   });
 
@@ -125,7 +127,7 @@ describe('Product Details Component', () => {
       }
     }).mockResolvedValueOnce({ data: { products: [ ] } });
 
-    const { getByPlaceholderText, getByText } = render(
+    const { getByText } = render(
       <MemoryRouter initialEntries={['/product/laptop']}>
           <Routes>
               <Route path="/product/:slug" element={<ProductDetails />} />
@@ -139,14 +141,14 @@ describe('Product Details Component', () => {
     expect(await screen.findByText('No Similar Products found')).toBeInTheDocument();
   });
 
-  it('should render similar products', async () => {
+  it('should render similar product', async () => {
     axios.get.mockResolvedValueOnce({
       data: {
         product: LAPTOP,
       }
     }).mockResolvedValueOnce({ data: { products: [ SMARTPHONE ] } });
 
-    const { getByPlaceholderText, getByText } = render(
+    const { getByText } = render(
       <MemoryRouter initialEntries={['/product/laptop']}>
           <Routes>
               <Route path="/product/:slug" element={<ProductDetails />} />
@@ -162,6 +164,32 @@ describe('Product Details Component', () => {
     expect(await screen.findByText('A high-end smartphone...')).toBeInTheDocument();
   });
 
+  it('should render similar products', async () => {
+    axios.get.mockResolvedValueOnce({
+      data: {
+        product: LAPTOP,
+      }
+    }).mockResolvedValueOnce({ data: { products: [ TELEVISION, SMARTPHONE ] } });
+
+    const { getByText } = render(
+      <MemoryRouter initialEntries={['/product/laptop']}>
+          <Routes>
+              <Route path="/product/:slug" element={<ProductDetails />} />
+          </Routes>
+      </MemoryRouter>
+    );
+
+    await waitFor(() => expect(axios.get).toHaveBeenCalledTimes(2));
+
+    expect(getByText('Similar Products ➡️')).toBeInTheDocument();
+    expect(await screen.findByText('Smartphone')).toBeInTheDocument();
+    expect(await screen.findByText('$999.99')).toBeInTheDocument();
+    expect(await screen.findByText('A high-end smartphone...')).toBeInTheDocument();
+    expect(await screen.findByText('Television')).toBeInTheDocument();
+    expect(await screen.findByText('$400.00')).toBeInTheDocument();
+    expect(await screen.findByText('A wide screen television...')).toBeInTheDocument();
+  });
+
   it('should navigate to product details page of similar product', async () => {
     axios.get.mockResolvedValueOnce({
       data: {
@@ -175,7 +203,7 @@ describe('Product Details Component', () => {
       }
     }).mockResolvedValueOnce({ data: { products: [ LAPTOP ] } });
 
-    const { getByPlaceholderText, getByText } = render(
+    const { getByText } = render(
       <MemoryRouter initialEntries={['/product/laptop']}>
           <Routes>
               <Route path="/product/:slug" element={<ProductDetails />} />
@@ -195,10 +223,12 @@ describe('Product Details Component', () => {
     expect(getByText('ADD TO CART')).toBeInTheDocument();
   });
 
-  it('should still render placeholder text on error with getting product details', async () => {
-    axios.get.mockRejectedValueOnce();
+  it('should gracefully handle error with getting product details', async () => {
+    const consoleSpy = jest.spyOn(console, "log").mockImplementation(() => {});
+    const errorMsg = "Error getting product details";
+    axios.get.mockRejectedValueOnce(errorMsg);
 
-    const { getByPlaceholderText, getByText } = render(
+    const { getByText } = render(
       <MemoryRouter initialEntries={['/product/laptop']}>
           <Routes>
               <Route path="/product/:slug" element={<ProductDetails />} />
@@ -207,6 +237,9 @@ describe('Product Details Component', () => {
     );
 
     await waitFor(() => expect(axios.get).toHaveBeenCalledTimes(1));
+
+    await waitFor(() => expect(consoleSpy).toHaveBeenCalledWith(errorMsg));
+    consoleSpy.mockRestore();
 
     expect(getByText('Product Details')).toBeInTheDocument();
     expect(getByText('Name :')).toBeInTheDocument();
@@ -218,14 +251,16 @@ describe('Product Details Component', () => {
     expect(getByText('No Similar Products found')).toBeInTheDocument();
   });
 
-  it('should still render product details on error with getting similar products', async () => {
+  it('should gracefully handle error with getting similar products', async () => {
+    const consoleSpy = jest.spyOn(console, "log").mockImplementation(() => {});
+    const errorMsg = "Error getting similar product";
     axios.get.mockResolvedValueOnce({
       data: {
         product: LAPTOP,
       }
-    }).mockRejectedValueOnce();
+    }).mockRejectedValueOnce(errorMsg);
 
-    const { getByPlaceholderText, getByText } = render(
+    const { getByText } = render(
       <MemoryRouter initialEntries={['/product/laptop']}>
           <Routes>
               <Route path="/product/:slug" element={<ProductDetails />} />
@@ -235,11 +270,35 @@ describe('Product Details Component', () => {
 
     await waitFor(() => expect(axios.get).toHaveBeenCalledTimes(2));
 
+    await waitFor(() => expect(consoleSpy).toHaveBeenCalledWith(errorMsg));
+    consoleSpy.mockRestore();
+
     expect(getByText('Product Details')).toBeInTheDocument();
     expect(await screen.findByText('Name : Laptop')).toBeInTheDocument();
     expect(await screen.findByText('Description : A powerful laptop')).toBeInTheDocument();
     expect(await screen.findByText('Price :$1,499.99')).toBeInTheDocument();
     expect(await screen.findByText('Category : Electronics')).toBeInTheDocument();
+    expect(getByText('ADD TO CART')).toBeInTheDocument();
+    expect(getByText('Similar Products ➡️')).toBeInTheDocument();
+    expect(getByText('No Similar Products found')).toBeInTheDocument();
+  });
+
+  it('should render placeholder text when no slug is present', async () => {
+    const { getByText } = render(
+      <MemoryRouter initialEntries={['/product']}>
+          <Routes>
+              <Route path="/product" element={<ProductDetails />} />
+          </Routes>
+      </MemoryRouter>
+    );
+
+    expect(axios.get).toHaveBeenCalledTimes(0);
+
+    expect(getByText('Product Details')).toBeInTheDocument();
+    expect(getByText('Name :')).toBeInTheDocument();
+    expect(getByText('Description :')).toBeInTheDocument();
+    expect(getByText('Price :')).toBeInTheDocument();
+    expect(getByText('Category :')).toBeInTheDocument();
     expect(getByText('ADD TO CART')).toBeInTheDocument();
     expect(getByText('Similar Products ➡️')).toBeInTheDocument();
     expect(getByText('No Similar Products found')).toBeInTheDocument();
