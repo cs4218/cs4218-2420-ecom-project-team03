e7d7998a1ccbea1c1fdeb5f04daa76f52bba4292
McNaBry <mcnabry123@gmail.com>
2025-03-09 03:03:48 +0800

    Add unit tests for Products page

---
+++ client/src/pages/admin/Products.test.js
@@ -0,0 +1,129 @@
+import { expect, jest } from "@jest/globals";
+import React from "react";
+import {
+  render,
+  waitFor,
+  screen
+} from "@testing-library/react";
+import axios from "axios";
+import Products from "./Products";
+import toast from "react-hot-toast";
+import { MemoryRouter } from "react-router-dom";
+import "@testing-library/jest-dom";
+
+jest.mock("axios");
+jest.mock("react-hot-toast");
+
+jest.mock("../../context/auth", () => ({
+  useAuth: jest.fn(() => [null, jest.fn()]),
+}));
+
+jest.mock("../../context/cart", () => ({
+  useCart: jest.fn(() => [null, jest.fn()]),
+}));
+
+jest.mock("../../context/search", () => ({
+  useSearch: jest.fn(() => [{ keyword: "" }, jest.fn()]),
+}));
+
+jest.mock("../../hooks/useCategory", () => jest.fn(() => []));
+describe("Products Component", () => {
+  const mockProducts = [ 
+    {
+      _id: '1',
+      name: "Laptop",
+      slug: "laptop",
+      description: "A powerful laptop",
+      price: 1499.99,
+      category: {
+        _id: "1",
+        name: "Electronics",
+        slug: "electronics"
+      }
+    },
+    {
+      _id: '2',
+      name: "Smartphone",
+      slug: "smartphone",
+      description: "A high-end smartphone",
+      price: 999.99,
+      category: {
+        _id: "1",
+        name: "Electronics",
+        slug: "electronics"
+      }
+    },
+    {
+      _id: '3',
+      name: "Textbook",
+      slug: "textbook",
+      description: "CS4218 Textbook in mint condition.",
+      price: 400,
+      category: {
+        _id: "2",
+        name: "Book",
+        slug: "book"
+      }
+    } 
+  ];
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("fetches product data on render", async () => {
+    axios.get.mockResolvedValueOnce({
+      data: { success: true, products: mockProducts },
+    });
+
+    const { getByPlaceholderText, getByText } = render(
+      <MemoryRouter>
+        <Products />
+      </MemoryRouter>
+    );
+    await waitFor(() => {
+      expect(getByText("All Products List")).toBeInTheDocument();
+      expect(axios.get).toHaveBeenCalledWith("/api/v1/product/get-product");
+    });
+  });
+
+  it("displays fetched products", async () => {
+    axios.get.mockResolvedValueOnce({
+      status: 200, data: { success: true, products: mockProducts },
+    });
+
+    render(
+      <MemoryRouter>
+        <Products />
+      </MemoryRouter>
+    );
+    
+    await waitFor(() => expect(axios.get).toHaveBeenCalledTimes(1));
+
+    await Promise.all(
+      mockProducts.map(async (item, index) => {
+        expect(await screen.findByText(item.name)).toBeInTheDocument();
+        expect(await screen.findByText(item.description)).toBeInTheDocument();
+        const productLink = screen.getByTestId(item._id);
+        expect(productLink).toHaveAttribute("href", `/dashboard/admin/product/${item.slug}`);
+        const productImage = await screen.findByAltText(item.name);
+        expect(productImage).toHaveAttribute("src", `/api/v1/product/product-photo/${item._id}`);
+      })
+    );
+  });
+
+  it("displays an error message if fetching products is unsuccessful", async () => {
+    axios.get.mockRejectedValueOnce("Product fetch error");
+
+    render(
+      <MemoryRouter>
+        <Products />
+      </MemoryRouter>
+    );
+
+    await waitFor(() => {
+      expect(axios.get).toHaveBeenCalledTimes(1);
+      expect(toast.error).toHaveBeenCalledWith("Something went wrong in getting products");
+    });
+  });
+});
