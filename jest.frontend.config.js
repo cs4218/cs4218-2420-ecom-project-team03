4cb6ec69133a3113114e7d17ccacbff361a5f991
limcaaarl <carljovi22@gmail.com>
2025-03-08 23:54:12 +0800

    Update config to include createCategory in coverage

--- jest.frontend.config.js
+++ jest.frontend.config.js
@@ -21,12 +21,12 @@ export default {
 
   // only run these tests
   testMatch: [
-    "<rootDir>/client/src/pages/Auth/*.test.js", "<rootDir>/client/src/pages/admin/CreateCategory.test.js",
+    "<rootDir>/client/src/pages/Auth/*.test.js", "<rootDir>/client/src/pages/admin/*.test.js",
   ],
 
   // jest code coverage
   collectCoverage: true,
-  collectCoverageFrom: ["client/src/pages/Auth/**"],
+  collectCoverageFrom: ["client/src/pages/Auth/**", "client/src/pages/admin/**"],
   coverageThreshold: {
     global: {
       lines: 100,
