b6e0a0b097d3311d4e86e87ff23d652b8ac90ac8
Lim Zi Jia <zijialim@gmail.com>
2025-03-10 03:00:23 +0800

    Contact tests

--- client/src/pages/Contact.test.js
+++ client/src/pages/Contact.test.js
@@ -1,8 +1,9 @@
 import React from "react";
+import Layout from "./../components/Layout";
 import { MemoryRouter } from "react-router-dom";
 import { render, screen } from "@testing-library/react";
 import '@testing-library/jest-dom/extend-expect';
-import Policy from "./Policy";
+import Contact from "./Contact";
 
 jest.mock('../context/auth', () => ({
   useAuth: jest.fn(() => [null, jest.fn()])
@@ -16,17 +17,20 @@ jest.mock('../context/search', () => ({
   useSearch: jest.fn(() => [{ keyword: '' }, jest.fn()])
 }));
 
-describe("Policy Component", () => {
-    it("Should render Policy with image and policies", () => {
+describe("Contact Component", () => {
+    it("Should render Contact with contact information", () => {
         render(
           <MemoryRouter>
-              <Policy />
+              <Contact />
           </MemoryRouter>);
-        
+
+        expect(screen.getByText("CONTACT US")).toBeInTheDocument();
         expect(screen.getByAltText("contactus")).toBeInTheDocument();
         expect(screen.getByAltText("contactus")).toHaveAttribute("src", "/images/contactus.jpeg");
 
-        const privacy_policies = screen.getAllByText("add privacy policy");
-        expect(privacy_policies).toHaveLength(7);
-    });
+        expect(screen.getByText("For any query or info about product, feel free to call anytime. We are available 24X7.")).toBeInTheDocument();
+        expect(screen.getByText(/www\.help@ecommerceapp\.com/)).toBeInTheDocument();
+        expect(screen.getByText(/012-3456789/)).toBeInTheDocument();
+        expect(screen.getByText(/1800-0000-0000 \(toll free\)/)).toBeInTheDocument();
+      });
 });
