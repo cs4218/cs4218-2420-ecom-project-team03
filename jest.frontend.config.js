866d772220fe07c8f8a8ff8a6b81b7e59e626f3f
limcaaarl <42115432+limcaaarl@users.noreply.github.com>
2025-03-09 22:41:33 +0800

    Unit Test for Create Category component (#36)
    * Add unit tests for createCategory
    
    * Add more test cases
    
    * Add toast for success:false for getAllCategory
    
    * Update config to include createCategory in coverage
    
    * Add test case to handle success:false in getAllCategory
    
    * Unit Test for Category controller (#3)
    
    * Update package.json and jest config
    
    * Using files provided in lab2
    
    * Add unit tests for categoryController
    
    * Fix wrong status
    
    * Fix bug when updating to same category name
    
    * Update return messages
    
    * Add new test cases
    
    * Fix empty string bug for update
    
    * Update single getter to include 404
    
    * Add name validation for whitespace only
    
    ---------
    
    Co-authored-by: McNaBry <mcnabry123@gmail.com>
    
    * Unit Test for Profile (#21)
    
    * Fix typos
    
    * Update config
    
    * Fix typos
    
    * Add unit tests
    
    * Unit Test for Users admin page (#20)
    
    * Update config
    
    * Add unit test
    
    * Unit Test for Category hook (#17)
    
    * Add unit tests
    
    * Update getCategories to check for success status
    
    * Add test case for success false
    
    * Unit Test for Categories component (#16)
    
    * Add unit tests
    
    * Update test cases to include all categories
    
    * Add unit test (#15)
    
    ---------
    
    Co-authored-by: McNaBry <mcnabry123@gmail.com>

--- jest.frontend.config.js
+++ jest.frontend.config.js
@@ -23,11 +23,24 @@ export default {
   testMatch: [
     "<rootDir>/client/src/pages/Auth/*.test.js",
     "<rootDir>/client/src/components/*.test.js",
+    "<rootDir>/client/src/components/Form/*.test.js",
+    "<rootDir>/client/src/pages/*.test.js",
+    "<rootDir>/client/src/hooks/*.test.js",
+    "<rootDir>/client/src/pages/admin/*.test.js",
+    "<rootDir>/client/src/pages/user/*.test.js",
   ],
-
   // jest code coverage
   collectCoverage: true,
-  collectCoverageFrom: ["client/src/pages/Auth/**", "client/src/components/**"],
+  collectCoverageFrom: [
+    "client/src/pages/Auth/**", 
+    "client/src/components/**", 
+    "client/src/pages/admin/**", 
+    "client/src/pages/user/**", 
+    "client/src/hooks/**", 
+    "client/src/pages/**",
+    "client/src/components/Form/**"
+  ],
+
   coverageThreshold: {
     global: {
       lines: 100,
