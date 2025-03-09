d0eca067ff3aecb8a4efe53e993fa4a63125757a
limcaaarl <carljovi22@gmail.com>
2025-03-09 14:54:19 +0800

    Fix typos

--- client/src/pages/user/Profile.js
+++ client/src/pages/user/Profile.js
@@ -34,7 +34,7 @@ const Profile = () => {
         phone,
         address,
       });
-      if (data?.errro) {
+      if (data?.error) {
         toast.error(data?.error);
       } else {
         setAuth({ ...auth, user: data?.updatedUser });
@@ -66,7 +66,7 @@ const Profile = () => {
                     value={name}
                     onChange={(e) => setName(e.target.value)}
                     className="form-control"
-                    id="exampleInputEmail1"
+                    id="exampleInputName1"
                     placeholder="Enter Your Name"
                     autoFocus
                   />
