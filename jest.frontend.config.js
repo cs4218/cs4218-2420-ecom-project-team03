aa0df12cfc4159de319717c1bd77748f5417b4ec
McNaBry <mcnabry123@gmail.com>
2025-03-09 18:18:08 +0800

    Add unit tests for create product page

--- jest.frontend.config.js
+++ jest.frontend.config.js
@@ -23,11 +23,12 @@ export default {
   testMatch: [
     "<rootDir>/client/src/pages/Auth/*.test.js",
     "<rootDir>/client/src/components/*.test.js",
+    "<rootDir>/client/src/pages/admin/*.test.js",
   ],
 
   // jest code coverage
   collectCoverage: true,
-  collectCoverageFrom: ["client/src/pages/Auth/**", "client/src/components/**"],
+  collectCoverageFrom: ["client/src/pages/Auth/**", "client/src/components/**", "client/src/pages/admin/**"],
   coverageThreshold: {
     global: {
       lines: 100,
