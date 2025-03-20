c48d813e0e94d211d82bcf0216f92da0047288d1
Bryan Lee <mcnabry123@gmail.com>
2025-03-21 00:09:14 +0800

    Add integrations tests for productController (#64)
    * Add basic dependencies for backend integration testing
    
    * Add mongodb memory server to create lightweight local instance
    * Add cross-env to set NODE_ENV on all OS platforms for testing
    * Add supertest to allow express API testing
    
    * Add integration tests for productController
    
    * Update unit tests for productController
    
    * Fix jwt signing for productController integration tests

--- config/db.js
+++ config/db.js
@@ -1,6 +1,11 @@
 import mongoose from "mongoose";
 import colors from "colors";
 const connectDB = async () => {
+    if (process.env.NODE_ENV == "test") {
+        console.log("Test environment detected. Not opening actual database connection.")
+        return;
+    }
+
     try {
         const conn = await mongoose.connect(process.env.MONGO_URL);
         console.log(`Connected To Mongodb Database ${conn.connection.host}`.bgMagenta.white);
