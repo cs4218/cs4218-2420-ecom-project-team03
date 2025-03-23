654174c565bd9769829bc2de9d0575979a2953b7
limcaaarl <carljovi22@gmail.com>
2025-03-24 01:08:44 +0800

    Add dummy data

---
+++ tests/admin/dummyData.js
@@ -0,0 +1,30 @@
+export const ADMIN_USER = {
+    email: "cs4218@test.com",
+    password: "cs4218@test.com"
+};
+
+export const DUMMY_PRODUCT = {
+    name: "test-product",
+    description: "this is a test product",
+    price: "20.4",
+    quantity: "40",
+    shipping: 'Yes'
+};
+
+export const UPDATED_DUMMY_PRODUCT = {
+    name: "test-product-2",
+    description: "this is not a test product",
+    price: "200",
+    quantity: "1",
+    shipping: 'No'
+};
+
+export const DUMMY_CATEGORY = {
+    name: "new category"
+}
+export const DUPLICATE_DUMMY_CATEGORY = {
+    name: "duplicate"
+}
+export const UPDATED_DUMMY_CATEGORY = {
+    name: "new category1"
+}
